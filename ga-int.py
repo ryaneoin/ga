f557cfbbbee913622fdffdbf1bab2a4e17f57c32
ryaneoin <30365993+ryaneoin@users.noreply.github.com>
2021-02-27 13:20:26 +0800

    ga example

---
+++ ga-int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+
+def f(X):
+    return np.sum(X)
+
+
+varbound=np.array([[0,10]]*3)
+
+model=ga(function=f,dimension=3,variable_type='int',variable_boundaries=varbound)
+
+model.run()
